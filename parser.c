7180cb41a0d1a776555b341375c68027f38eed2c
Stanley Lin <slin55055@gmail.com>
2018-04-17 22:16:53 -0400

    it works

--- parser.c
+++ parser.c
@@ -1,7 +1,6 @@
 #include"include/parser.h"
 
-void parse_instructions(char *filename, struct Matrix *t,
-			struct Matrix *e, struct Matrix *p, Frame f) {
+void parse_instructions(char *filename, struct Rcs_stack *s, Frame f) {
 	FILE *file = fopen(filename, "r");
 	if (!file) return;
 	
@@ -9,6 +8,14 @@ void parse_instructions(char *filename, struct Matrix *t,
 	char name[128];
 	float x1, y1, z1, x2, y2, z2;
 	char axis;
+	
+	struct Matrix *e = new_matrix(4, 1);
+	struct Matrix *p = new_matrix(4, 1);
+	struct Matrix *t = new_matrix(4, 4);
+	
+	memset(f, 0, sizeof(Frame));
+	struct Pixel pixel;
+	pixel_color(&pixel, 255, 105, 180);
 	
 	while (fgets(line, sizeof(line), file) != 0) {
 		if (!strncmp(line, "line", strlen(line)-1)) {
@@ -21,26 +28,45 @@ void parse_instructions(char *filename, struct Matrix *t,
 			fgets(line, sizeof(line), file);
 			sscanf(line, "%f %f %f",
 				&x1, &y1, &z1);
-			scale(t, x1, y1, z1);
+			
+			struct Matrix *t = scale(x1, y1, z1);
+			//matrix_mult(t, s->stack[s->top]);
+			
+			matrix_mult(s->stack[s->top], t);
+			free_matrix(s->stack[s->top]);
+			s->stack[s->top] = copy_matrix(t);
+			
+			free_matrix(t);
 		}
 		else if (!strncmp(line, "move", strlen(line)-1)) {
 			fgets(line, sizeof(line), file);
 			sscanf(line, "%f %f %f",
 				&x1, &y1, &z1);
-			move(t, x1, y1, z1);
+			
+			struct Matrix *t = move(x1, y1, z1);
+			//matrix_mult(t, s->stack[s->top]);
+			
+			matrix_mult(s->stack[s->top], t);
+			free_matrix(s->stack[s->top]);
+			s->stack[s->top] = copy_matrix(t);
+			
+			free_matrix(t);
 		}
 		else if (!strncmp(line, "rotate", strlen(line)-1)) {
 			fgets(line, sizeof(line), file);
 			sscanf(line, "%c %f",
 				&axis, &z1);
-			rotate(t, axis, z1);
+			
+			struct Matrix *t = rotate(axis, z1);
+			//matrix_mult(t, s->stack[s->top]);
+			
+			matrix_mult(peek(s), t);
+			free_matrix(s->stack[s->top]);
+			s->stack[s->top] = copy_matrix(t);
+			
+			free_matrix(t);
 		}
 		else if (!strncmp(line, "display", strlen(line)-1)) {
-			memset(f, 0, sizeof(Frame));
-			struct Pixel pixel;
-			pixel_color(&pixel, 255, 105, 180);
-			draw_lines(f, e, &pixel);
-			draw_polygons(f, p, &pixel);
 			display(f);
 		}
 		else if (!strncmp(line, "save", strlen(line)-1)) {
@@ -76,32 +102,58 @@ void parse_instructions(char *filename, struct Matrix *t,
 		}
 		else if (!strncmp(line, "sphere", strlen(line)-1)) {
 			float x, y, z, r;
+			struct Matrix *p = new_matrix(4, 1);
+			
 			fgets(line, sizeof(line), file);
 			sscanf(line, "%f %f %f %f",
 				      &x, &y, &z, &r);
+			
+			//draw sphere then remove the matrix
 			add_sphere(p, x, y, z, r, 8);
+			matrix_mult(s->stack[s->top], p);
+			draw_polygons(f, p, &pixel);
+			free_matrix(p);
+			print_stack(s);
 		}
 		else if (!strncmp(line, "box", strlen(line)-1)) {
 			float x, y, z, w, h, d;
+			struct Matrix *p = new_matrix(4, 1);
+			
 			fgets(line, sizeof(line), file);
 			sscanf(line, "%f %f %f %f %f %f",
 				      &x, &y, &z, &w, &h, &d);
+			
 			add_cube(p, x, y, z, w, h, d);
+			matrix_mult(s->stack[s->top], p);
+			draw_polygons(f, p, &pixel);
+			free_matrix(p);
+			print_stack(s);
 		}
 		else if (!strncmp(line, "torus", strlen(line)-1)) {
 			float x, y, z, r1, r2;
+			struct Matrix *p = new_matrix(4, 1);
+			
 			fgets(line, sizeof(line), file);
 			sscanf(line, "%f %f %f %f %f",
 				      &x, &y, &z, &r1, &r2);
+			
 			add_torus(p, x, y, z, r1, r2, 10);
+			matrix_mult(s->stack[s->top], p);
+			draw_polygons(f, p, &pixel);
+			free_matrix(p);
+			print_stack(s);
 		}
 		else if (!strncmp(line, "push", strlen(line)-1)) {
-			
+			push_rcs(s);
 		}
 		else if (!strncmp(line, "pop", strlen(line)-1)) {
-			
+			pop_rcs(s);
 		}
 	}
+	
+	free_matrix(e);
+	free_matrix(p);
+	free_matrix(t);
 	
 	fclose(file);
 	file = 0;
